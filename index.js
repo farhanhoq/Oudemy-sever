adc7818af21c4c46d40d52f26d4a0ed399c3fde8
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-10-27 09:49:45 +0600

    load coursenames

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 const cors = require('cors');
 const port = process.env.Port || 5000;
 
+const coursename = require('./data/coursename.json')
+
+app.get('/', (req, res) => {
+    res.send('Running')
+})
+
+app.get('/coursename', (req, res) => {
+    res.send(coursename)
+})
+
 
 app.listen(port, () => {
     console.log(port)
